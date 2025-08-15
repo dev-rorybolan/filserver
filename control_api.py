6c36af19eefcb6f81b0e9d3dca1c6c8a52a078fb
Belgarion <dev.rorybolan@gmail.com>
2025-08-15 13:31:17 +0200

    Update control_api.py

--- control_api.py
+++ control_api.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, send_file
 import subprocess
 import os
+from moviepy.editor import VideoFileClip
 app = Flask(__name__)
 def download(data):
     path = None
@@ -12,8 +13,25 @@ def download(data):
     return send_file(f"/Users/rorybolan/Desktop/Download_files/{name}", as_attachment=True)
 
 
+def return_files() -> dict:
+    
+    folder_path = "/Users/rorybolan/Desktop/Download_files"
 
-
+    names = []
+    lengths = []
+    
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".mp4"):
+            file_path = os.path.join(folder_path, filename)
+            names.append(filename)
+            
+            clip = VideoFileClip(file_path)
+            lengths.append(clip.duration)  
+            clip.close() 
+    return {
+    "names": names,
+    "lengths": lengths
+    }
 @app.route("/command", methods=["POST"])
 def run_command():
     data = request.get_json(silent=True) or {}
@@ -23,6 +41,8 @@ def run_command():
     match data["command"]:
         case 10:
             return download(data)
+        case 20:
+            return return_files()
         case _:
             return {"status": "failed"}, 400
 if __name__ == "__main__":
